1fa0289ef067bd6f389eb6c7e32204c4a0ba148a
hicham <hicham.34@hotmail.fr>
2019-01-14 12:51:04 +0100

    calculator2

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -1,16 +1,25 @@
 def add(chiffre1, chiffre2)
+    return chiffre1 + chiffre2
 
 end
 
-def sum(chiffre1, chiffre2)
+def sum(tableau)
+    return tableau.sum
+
+end
+
+def subtract(chiffre1, chiffre2)
+    return chiffre1 - chiffre2
 
 end
 
 def multiply(chiffre1, chiffre2)
+    return chiffre1 * chiffre2
 
 end
 
-def power
+def power(chiffre1, puissance) 
+    return chiffre1 = chiffre1**puissance
 
 end
 
