ed9fca260f8e339ac947bae0663e211dee1f44f9
hicham <hicham.34@hotmail.fr>
2019-01-14 14:33:15 +0100

    Exo 4 debut

---
+++ lib/04_simon_says.rb
@@ -0,0 +1,23 @@
+def echo(text)
+    return text 
+
+end
+
+def shout(text)
+    return text.upcase
+end
+
+
+def repeat(text,number=2)
+        
+        return ([text] * number).join " "
+    
+end 
+
+
+def start_of_word
+end
+
+
+def first_word
+end
